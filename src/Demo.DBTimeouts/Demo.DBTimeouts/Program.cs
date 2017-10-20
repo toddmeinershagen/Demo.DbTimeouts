b804d471f3bf0cc51a9cb60f3258c86f5d3e6cb0
Todd Meinershagen <todd@meinershagen.net>
2017-10-20 14:26:14 -0500

    Initial commit

---
+++ src/Demo.DBTimeouts/Demo.DBTimeouts/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+
+namespace Demo.DBTimeouts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var context = new TestContext())
+                {
+                    //context.Database.CommandTimeout = 7;
+                    context.Database.ExecuteSqlCommand("exec dbo.DelayInSeconds @delayInSeconds = 6");
+                    //context.Database.CommandTimeout = 30;
+                }
+
+                Console.WriteLine("Success!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failure:  \r\n\r\n{e.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class TestContext : System.Data.Entity.DbContext
+    {
+        public TestContext() :
+            base("TestContext")
+        {
+            var objectContext = (this as IObjectContextAdapter).ObjectContext;
+            objectContext.CommandTimeout = 2;
+        }
+    }
+}
---
+++ src/Demo.DBTimeouts/Demo.DBTimeouts/dbo.DelayInSeconds.sql
@@ -0,0 +1,17 @@
+﻿IF OBJECT_ID('dbo.DelayInSeconds','P') IS NOT NULL  
+    DROP PROCEDURE dbo.DelayInSeconds;  
+GO  
+CREATE PROCEDURE dbo.DelayInSeconds   
+    (  
+		@delayInSeconds int = 0
+    )  
+AS  
+	DECLARE @returnInfo varchar(255)  
+ 
+	BEGIN  
+		declare @delayLength varchar(8) = '00:00:' + right('00' + cast(@delayInSeconds as char(2)), 2)
+		WAITFOR DELAY @delayLength;  
+		SELECT @returnInfo = 'A total time of ' + @delayLength + ', hh:mm:ss, has elapsed! Your time is up.'   
+		PRINT @returnInfo;
+	END;  
+GO 
